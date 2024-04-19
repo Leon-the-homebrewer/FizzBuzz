0ced0eab265c051d6fb227eb9f0d4b104fa5b58b
Leon Goodwin <leongoodwin@Michaels-MacBook-Pro.local>
2024-04-19 11:20:09 -0400

    Add algorithm to find numbers divisble by 3, 5, or both

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///-----------------------------------------------------------------
+///   Description:  Fizz Buzz Game Challenge in C#
+///   Author:       Leon Goodwin
+///   Date:         19 April, 2024
+///-----------------------------------------------------------------
 
+//Loop
+for (int i=1 ;i<=100 ; i++) 
+{
+    //Check If Divisble By 3
+    if (i % 3 == 0)
+    {
+        //Check If Divisble By 5 And 3
+        if (i % 5 == 0)
+        {
+            //Write FizzBuzz If Divisble By 3 & 5
+            Console.WriteLine("FizzBuzz");
+        }
+        else
+        {
+            //Write Fizz If Only Divisble By 3
+            Console.WriteLine("Fizz");
+        }
+    }
+    //Check If Divisble By 5
+    else if (i % 5 == 0)
+    {
+        //Write Fizz If Only Divisble By 5
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        //Output If Not Divisble By 5 Or 3
+        Console.WriteLine(i);
+    }
+}
